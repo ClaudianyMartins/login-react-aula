9a40a6cf5d456037566a8191d49d5792f2aa4aa4
ClaudianyMartins <41713981+ClaudianyMartins@users.noreply.github.com>
2020-09-23 09:27:52 -0300

    Initial commit

---
+++ src/Components/Home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Bem vindo à Home </h1>
+      </div>
+    );
+  }
+}
