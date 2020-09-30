02ecb90fb39cd7c09f6ac1163268d1d9432593e5
ClaudianyMartins <41713981+ClaudianyMartins@users.noreply.github.com>
2020-09-30 09:16:24 -0300

    Theme com styled-component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./Components/Tema/GlobalStyles";
+import { lightTheme, darkTheme } from "./Components/Tema/Themes";
 import "./styles.css";
 import Routes from "./router";
 
 export default function App() {
+  const [theme, setTheme] = useState("light");
+
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <>
+        <GlobalStyles />
+        <div className="App">
+          <button className="btn" onClick={themeToggler}>
+            Alterar tema
+          </button>
+          <Routes />
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
